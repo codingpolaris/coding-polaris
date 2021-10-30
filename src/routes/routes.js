d2d9a98374ffa851e75feefefbde327114809951
Luiz Santiago <luiz.santiago@radixeng.com.br>
2021-10-29 23:11:14 -0300

    Added new pages to router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Login } from '../pages/login/login';
+import { Home } from '../pages/home/home';
+import { OnBoarding } from '../pages/onBoarding/onBoarding';
+import { SignUp } from '../pages/signUp/signUp';
+
 
 function Routes() {
   return (
     <BrowserRouter>
     <Switch>
-        <Route path='/' exact component={Login} />
+        <Route path='/' exact component={OnBoarding} />
+        <Route path='/login' exact component={Login} />
+        <Route path='/signUp' exact component={SignUp} />
       </Switch>
     </BrowserRouter>
   );
