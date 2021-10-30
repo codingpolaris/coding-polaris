50034a3f1230ca55424dfdc866d106fa0b1181a4
Luiz Santiago <luiz.santiago@radixeng.com.br>
2021-10-30 11:34:51 -0300

    Upgrade jeader component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,24 @@
 import logo from '../../assets/Logo.png';
 import styles from './header.module.scss';
+import { ReactComponent as ConfigIcon } from '../../assets/icons/Config.svg';
+import { ReactComponent as ContentIcon } from '../../assets/icons/Content.svg';
+import { ReactComponent as ProfileIcon } from '../../assets/icons/Profile.svg';
 
 interface Props {
-    isLogin: boolean;
-  }
+  isLogin: boolean;
+}
 
-export function Header(props:Props) {
-    
+export function Header(props: Props) {
   return (
-    <div className={styles.header}  >        
-        <img className={styles.logo} src={logo} alt="Logo"/>
-    </div>  
-    );
+    <div className={styles.header}>
+      <img className={styles.logo} src={logo} alt='Logo' />
+      {props.isLogin ? (
+        <div className={styles.buttonsMenu}>
+          <ContentIcon color='red' />
+          <ProfileIcon />
+          <ConfigIcon className={styles.icons} />
+        </div>
+      ) : null}
+    </div>
+  );
 }
