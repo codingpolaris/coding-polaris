27b1e5cb863cd136d3038fa37b8fe4cc3228319c
Bianca Barbosa <yume_shiro@hotmail.com>
2021-11-06 00:13:16 -0300

    feat: creating upload icon and photo area

--- src/pages/configuration/configuration.tsx
+++ src/pages/configuration/configuration.tsx
@@ -5,6 +5,7 @@ import { Button } from '../../components/button/button';
 import logo from '../../assets/Logo.png';
 import { Input } from '../../components/input/input';
 import { Card } from '../../components/card/card';
+import { ReactComponent as UploadIcon } from '../../assets/icons/Upload.svg';
 
 export function Configuration() {
     return (
@@ -12,6 +13,10 @@ export function Configuration() {
           <Header isLogin={true} selected={'Configuration'} />
             <div className={styles.buttonArea}>  
                 <Card> 
+                    <div className={styles.photoArea}>          
+                        <div className={styles.photo}></div>
+                        <UploadIcon className={styles.uploadButton}></UploadIcon>
+                    </div>
                     <Button name={'secundary'}>Alterar nome</Button>
                     <Button name={'secundary'}>Mudar senha</Button>
                     <Button name={'secundary'}>Modificar gênero</Button>
