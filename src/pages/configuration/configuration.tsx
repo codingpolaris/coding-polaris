0d7f11761289248583406a4bfe36028d1245f3c3
Bianca Barbosa <yume_shiro@hotmail.com>
2021-11-05 23:28:00 -0300

    feat: creating card component and adjusting config icon

--- src/pages/configuration/configuration.tsx
+++ src/pages/configuration/configuration.tsx
@@ -4,15 +4,18 @@ import { Header } from '../../components/header/header';
 import { Button } from '../../components/button/button';
 import logo from '../../assets/Logo.png';
 import { Input } from '../../components/input/input';
+import { Card } from '../../components/card/card';
 
 export function Configuration() {
     return (
         <div className={styles.container}>     
           <Header isLogin={true} selected={'Configuration'} />
-            <div className={styles.buttonArea}>          
-                <Button name={'secundary'}>Alterar nome</Button>
-                <Button name={'secundary'}>Mudar senha</Button>
-                <Button name={'secundary'}>Modificar gênero</Button>
+            <div className={styles.buttonArea}>  
+                <Card> 
+                    <Button name={'secundary'}>Alterar nome</Button>
+                    <Button name={'secundary'}>Mudar senha</Button>
+                    <Button name={'secundary'}>Modificar gênero</Button>
+                </Card>        
             </div>
         </div>
     );
