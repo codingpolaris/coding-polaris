d0b1afc9adb0422293293d4aceb43fc4023676b7
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-10 20:18:36 -0300

    bugfix: changing route

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/', 
+  baseURL: 'https://coding-polaris-api.herokuapp.com/', 
   //Se for testar utilizando emulador, inicie o ngrok na api e troque o link acima pelo do ngrok
 });
 
