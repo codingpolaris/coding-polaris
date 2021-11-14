7784188e511bb1f74f6ca44525444c525a51b1ab
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-14 00:55:50 -0300

    WIP: Integrating with back end

---
+++ src/components/themesComponent/themesComponent.tsx
@@ -0,0 +1,96 @@
+import styles from "./themesComponent.module.scss";
+import Api from "../../services/api";
+import { Button } from "../../components/button/button";
+import IThemes from "../../models/IThemes";
+import IContents from "../../models/iContents";
+import IChallenge from "../../models/iChallenge";
+
+import ICharacterThemeRequest from "../../models/requests/iCharacterThemeRequest";
+import { useEffect, useState } from "react";
+import { ReactComponent as Arrow } from "../../assets/icons/Arrow-1.svg";
+import { useHistory } from "react-router";
+
+export default function ThemesComponent(props: ICharacterThemeRequest) {
+  const history = useHistory();
+
+  const [themes, setThemes] = useState({} as IThemes[]);
+
+  let theme = [] as IThemes[];
+  let contents = [] as IContents[];
+  let challenges = [] as IChallenge[];
+
+
+  useEffect(() => {
+    async function getTheme() {
+      try {
+        const { data } = await Api.get(`theme-paths/${props.themeId}`);
+        data.forEach((themes: any) => {
+          theme.push(themes.theme);
+        });
+
+        setThemes(theme);
+      } catch (err) {
+        alert("ocorreu algum erro");
+      }
+    }
+    getTheme();
+  }, []);
+
+  async function getContent(id: number) {
+    try {
+        const { data } = await Api.get(`contents/${id}`);
+        contents=data;
+        history.push("/contents", { params: contents });
+      } catch (err) {
+        alert("ocorreu algum erro");
+      }
+    }
+
+    async function getChallenges(id: number) {
+        try {
+            const { data } = await Api.get(`challenges/${id}`);
+            challenges=  data;          
+            history.push("/questions", { params: challenges });
+          } catch (err) {
+            alert("ocorreu algum erro");
+          }
+        }
+
+  async function postTheme(themeId: number) {
+    try {
+      let request = {} as ICharacterThemeRequest;
+      request.characterId = props.characterId;
+      request.themeId = themeId;
+      await Api.post("character-theme", request);
+    } catch (err) {
+      alert("ocorreu algum erro ao salvar");
+    }
+  }
+
+  return (
+    <div>
+      {themes.length > 0 ? (
+        <div className={styles.container}>
+          <p className={styles.secondaryText}>Siga sua trilha</p>
+          {themes.map((theme: IThemes) => (
+            <div className={styles.buttonArea}>
+              <div className={styles.themeButton}>
+                <Button name={"primary"} onClick={() => getContent(theme.id)}>
+                  Conteúdo
+                </Button>
+                <Button name={"primary"} onClick={() => postTheme(theme.id)}>
+                  {theme.name}
+                </Button>
+                <Button name={"primary"} onClick={() => getChallenges(theme.id)}>
+                  Atividade
+                </Button>
+              </div>
+
+              <Arrow />
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,29 +1,75 @@
-import { useHistory } from 'react-router-dom';
-import styles from './home.module.scss';
-import Api from '../../services/api';
-import { Button } from '../../components/button/button';
-import { Header } from '../../components/header/header';
-import { ReactComponent as Arrow } from '../../assets/icons/Arrow 1.svg';
+import { useHistory, useLocation } from "react-router-dom";
+import styles from "./home.module.scss";
+import Api from "../../services/api";
+import { Header } from "../../components/header/header";
+import IUser from "../../models/iUser";
+import ICharacter from "../../models/iCharacter";
+import ICharacterPath from "../../models/iCharacterPath";
+import { useEffect, useState } from "react";
+import PathsComponent from "../../components/pathsComponent/pathsComponent";
+import ThemesComponent from "../../components/themesComponent/themesComponent";
+import ICharacterPathRequest from "../../models/requests/iCharacterPathRequest";
 
 export function Home() {
+  const { state } = useLocation<any>();
+  const user = state.params as IUser;
+  const [character, setCharacter] = useState({} as ICharacter);
+  const [characterPath, setCharacterPath] = useState({} as ICharacterPath[]);
+  const [noob, setNoob] = useState(false);
+
+  useEffect(() => {
+    async function getCharacter() {
+      try {
+        await Api.get(`characters/${user.id}`).then((resp) => {
+          console.log("Opa");
+          console.log(resp.data);
+          setCharacter(resp.data);
+          Api.get(`characters-paths/${resp.data.id}`).then((resp) => {
+            setCharacterPath(resp.data);
+            setNoob(!noob);
+            if (resp.data.length <= 0) {
+              const register = {} as ICharacterPathRequest;
+              register.characterId = character.id;
+              register.pathId = 1;
+              Api.post("characters-paths/", register);
+            }
+          });
+        });
+      } catch (err) {
+        alert("ocorreu algum erro no personagem");
+      }
+    }
+
+    getCharacter();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Header isLogin={true} selected={'Content'} />
+      <Header isLogin={true} selected={"Content"} />
       <div className={styles.textArea}>
         <span className={styles.welcomeText}>
-          Seja bem-vinda <span className={styles.name}>Alexa</span>
+          {" "}
+          {user.gender === "male" ? "Seja bem-vindo " : "Seja bem-vinda "}
+          <span className={styles.name}>{user.full_name}</span>
         </span>
+        {noob ? (
+          <div>
+            {characterPath.length <= 0 ? (
+              <div className={styles.textArea}>
+                <span className={styles.primaryText}>
+                  Agora chegou sua vez de se aprofundar em uma
+                  <span className={styles.stack}> stack</span>
+                </span>
+                <PathsComponent />
+              </div>
+            ) : <ThemesComponent themeId={characterPath[0].path.id} characterId={character.id}/>}
+          </div>
+        ) : null}
+
         <p className={styles.secondaryText}>
           Seu caminho para se tornar uma desenvolvedora começa aqui
         </p>
       </div>
-      <div className={styles.buttonArea}>
-        <Button name={'primary'}>Lógica de Programação</Button>
-        <Arrow/>
-        <Button name={'primary'}>Git</Button>
-        <Arrow/>
-      </div>
-      <p className={styles.endText}>Parabéns por chegar até aqui!</p>
     </div>
   );
 }
